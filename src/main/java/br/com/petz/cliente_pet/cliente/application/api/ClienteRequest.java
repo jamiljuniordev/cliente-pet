d9a0f8cf1c167052fcda01a13789e2d584d77ec7
Jamil Junior <jamil.backend@gmail.com>
2025-03-31 10:05:46 -0300

    implementa request e response

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,23 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
+
+import br.com.petz.cliente_pet.cliente.domain.Sexo;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public class ClienteRequest { private String nomeCompleto;
+@NotNull
+@Email
+private String email;
+@NotNull
+private String celular;
+private String telefone;
+private Sexo sexo;
+@NotNull
+private LocalDate dataNascimento;
+private String cpf;
+@NotNull
+private Boolean aceitaTermos;
 
 }
