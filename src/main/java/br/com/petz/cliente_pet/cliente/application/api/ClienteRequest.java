04270391be9f02328df9bcdadb8c0376cf23e39c
Jamil Junior <jamil.backend@gmail.com>
2025-03-31 10:49:01 -0300

    implementa ClienteApplicationService e corrige clienteAPI

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteRequest.java
@@ -1,10 +1,13 @@
 package br.com.petz.cliente_pet.cliente.application.api;
 
 import java.time.LocalDate;
-
+import org.hibernate.validator.constraints.br.CPF;
 import br.com.petz.cliente_pet.cliente.domain.Sexo;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.Value;
+
+@Value
 
 public class ClienteRequest { private String nomeCompleto;
 @NotNull
---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
@@ -0,0 +1,21 @@
+package br.com.petz.cliente_pet.cliente.application.service;
+import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
+
+import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
+import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class ClienteApplicationService implements ClienteService {
+
+	@Override
+	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
+		log.info("[inicia] ClienteApplicationService - criaCliente");
+		log.info("[finaliza] ClienteApplicationService - criaCliente");
+
+		return null;
+	}
+
+}
